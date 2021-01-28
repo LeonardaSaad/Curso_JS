963df7a1d2915fc9d3e924377d78f82dfdbf05b2
Leonarda Saad <leonardassaad777@gmail.com>
2021-01-28 17:04:43 -0300

    Adicionando aulas

---
+++ Sessão 4/Aula 71/index.js
@@ -0,0 +1,29 @@
+function criaPessoa(nome, sobrenome, altura, peso) {
+    return {
+        nome,
+        sobrenome,
+        // Getter
+        get nomeCompleto() {
+            return `${this.nome} ${this.sobrenome}`
+        },
+        // Setter
+        set nomeCompleto(valor) {
+            valor = valor.split(' ');
+            this.nome = valor.shift();
+            this.sobrenome = valor.join(' ');
+            console.log(valor)
+        },
+        fala(assunto) {
+            return `${this.nome} está ${assunto}.`;
+        },
+        altura,
+        peso,
+        get imc() {
+            const indice = this.peso / (this.altura ** 2);
+            return indice.toFixed(2);
+        }
+    }
+}
+const p1 = criaPessoa('Luiz', 'Otávio', 1.8, 80)
+p1.nomeCompleto = 'Maria Oliveira Silva'
+console.log(p1.nomeCompleto)
